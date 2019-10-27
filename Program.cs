4ff5730c275caab3b2fe6df665200a27ba872c4d
haoxiaofan <569192072@qq.com>
2019-10-27 19:35:19 +0800

    add commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace data_structure
 {
@@ -6,7 +7,21 @@ namespace data_structure
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var array = new[] { 5, 8, 9, 6, 3, 5, 4, 7, 5, 2, 9, 1, 0, 5, 2, 6, 7, 4 };
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        var temp = array[j];
+                        array[j] = array[i];
+                        array[i] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine(String.Join(",", array.ToList()));
         }
     }
 }
